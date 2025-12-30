cf7ffc3f811140fb1a67e9096052e354e3e4717c
HatidzaImamovic <hatidza12.imamovic12@gmail.com>
2025-12-30 18:35:10 +0100

    Refactor login tests for improved readability

--- src/tests/functional/login.spec.js
+++ src/tests/functional/login.spec.js
@@ -12,19 +12,19 @@ test.describe('Tests for Login', () => {
         await loginPage.navigate();
     });
 
-    test('Login with valid information', async ({page}) => {
+    test('Login with valid information', async () => {
         await loginPage.login('standard_user', 'secret_sauce');
 
         expect(await inventoryPage.visibleFullInventory()).toBe(6);
     });
 
-    test('Login with invalid information', async ({page}) => {
+    test('Login with invalid information', async () => {
         await loginPage.login('test_wrong', 'wrong_pass');
 
         expect(await loginPage.getErrorMsg()).toContain('Epic sadface');
     });
 
-    test('Login with empty fields', async ({page}) => {
+    test('Login with empty fields', async () => {
         await loginPage.login('', '');
 
         expect(await loginPage.getErrorMsg()).toContain('is required');
